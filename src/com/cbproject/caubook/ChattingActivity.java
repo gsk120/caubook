c2f70b2e22ebc343538079f7846ca7d4df4a6790
gsk120 <gsk120@naver.com>
2015-01-25 00:08:45 +0900

    채팅 레이아웃 얼추 구성 완료

--- src/com/cbproject/caubook/ChattingActivity.java
+++ src/com/cbproject/caubook/ChattingActivity.java
@@ -1,6 +1,8 @@
 package com.cbproject.caubook;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import android.content.Context;
@@ -8,15 +10,20 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ChattingActivity extends ActionBarActivity {
+public class ChattingActivity extends ActionBarActivity implements OnClickListener{
 
 	private ListView listChatting;
 	private MessageListAdpater listChattingAdapter;
+	private Button btnSend;
+	private EditText inputMessage;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +31,27 @@ public class ChattingActivity extends ActionBarActivity {
 	    setContentView(R.layout.a_chatting);
 	    
 	    listChatting = (ListView)findViewById(R.id.list_chatting_log);
+	    listChattingAdapter = new MessageListAdpater(getApplicationContext());
+	    listChatting.setAdapter(listChattingAdapter);
 	    
+	    btnSend = (Button)findViewById(R.id.btn_chatting_send);
+	    btnSend.setOnClickListener(this);
 	    
+	    inputMessage = (EditText)findViewById(R.id.edit_chatting_input);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.btn_chatting_send:
+			Calendar calendar = Calendar.getInstance();
+			MessageData sendData = new MessageData(inputMessage.getEditableText().toString(), calendar.getTime());
+			sendData.setMessageType(MessageTypeEnum.SendMsg);
+			listChattingAdapter.addItem(sendData);
+			// NOTE 리스트뷰 리셋 하는부분
+			listChatting.setAdapter(listChattingAdapter);
+			break;
+		}
 	}
 	
 	private class MessageData {
@@ -42,6 +68,8 @@ public class ChattingActivity extends ActionBarActivity {
 		public String getMessageContent() { return this.msgContent; }
 		public Date getMessageTime() { return this.msgTime; }
 		public MessageTypeEnum getMessageType() { return this.msgType; }
+		
+		public void setMessageType(MessageTypeEnum _type) { this.msgType = _type; }
 	}
 	
 	private class MessageVeiwHolder {
@@ -102,7 +130,8 @@ public class ChattingActivity extends ActionBarActivity {
 			}
 			
 			holder.getContentView().setText(data.getMessageContent());
-			holder.getTimeView().setText(data.getMessageTime().toString());
+			SimpleDateFormat dateFormat = new SimpleDateFormat("a hh:mm");
+			holder.getTimeView().setText(dateFormat.format(data.getMessageTime()));
 			
 			return convertView;
 		}
