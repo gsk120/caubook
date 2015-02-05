18edd4059b9808a578ce0afe2b27445fc28f73b1
gsk120 <gsk120@naver.com>
2015-02-06 03:33:37 +0900

    채팅 sqlite에 저장하기 완료 이제 버그 수정 해야함

--- src/com/cbproject/caubook/activities/ChattingActivity.java
+++ src/com/cbproject/caubook/activities/ChattingActivity.java
@@ -7,10 +7,15 @@ import java.util.Date;
 
 import com.cbproject.caubook.MessageTypeEnum;
 import com.cbproject.caubook.R;
+import com.cbproject.caubook.sqlite.ConversationTable;
+import com.cbproject.caubook.sqlite.ConversationTable.ConversationData;
+import com.cbproject.caubook.sqlite.UserTable;
+import com.cbproject.caubook.sqlite.UserTable.UserData;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +33,12 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 	private Button btnSend;
 	private EditText inputMessage;
 	
+	int i=0; //테스트용ㄴ
+
+	// 채팅 목록 sqlite에 저장하기 위해 DB관련 멤버변수 선언
+	private UserTable userTable;
+	private ConversationTable conversationTable;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -44,6 +55,25 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 	    
 	    String userID = getIntent().getStringExtra("userID");
 	    setTitle(userID);
+	    
+	    // 데이터 베이스 관련 변수 초기화
+	    userTable = new UserTable(this);
+	    conversationTable = new ConversationTable(this);
+	    
+	    if(!userTable.isExist(userID)) {
+	    	// 나와 대화 상대로 상대 아이디가 있으면 새로 추가
+	    	UserData user = new UserData(0, userID);
+		    userTable.insertQuery(user);
+	    } else {
+	    	// 나와 대화ㄴ한적이 있으면 채팅 리스트 불러오기
+	    	ArrayList<ConversationData> conversationList = conversationTable.selectAllQuery();
+	    	for(int i=0; i<conversationList.size(); i++) {
+	    		Log.i("dd", "ddddddddddd");
+	    		MessageData data = new MessageData(conversationList.get(i).getContent(), conversationList.get(i).getTime());
+	    		listChattingAdapter.addItem(data);
+	    		listChattingAdapter.notifyDataSetChanged();
+	    	}
+	    }
 	}
 
 	@Override
@@ -60,10 +90,13 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 			listChattingAdapter.addItem(sendData);
 			listChattingAdapter.notifyDataSetChanged();
 			inputMessage.setText(null);
+			ConversationData data = new ConversationData(0, i++, sendData.getMessageContent(), sendData.getMessageTime(), sendData.getMessageType());
+			conversationTable.insertQuery(data);
 			break;
 		}
 	}
 	
+	// 채팅 내용 데이터 클래스 내용, 송수신 시간, 송수신 메세지 타입을 저장
 	private class MessageData {
 		private String msgContent;
 		private Date msgTime;
@@ -82,6 +115,7 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 		public void setMessageType(MessageTypeEnum _type) { this.msgType = _type; }
 	}
 	
+	// 메세지의 뷰 관리 클래스
 	private class MessageVeiwHolder {
 		private TextView tvContent;
 		private TextView tvTime;
@@ -93,6 +127,7 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 		public void setTimeView(TextView _time) { this.tvTime = _time; }
 	}
 	
+	// 채팅리스트의 컨트롤 클래스
 	private class MessageListAdpater extends BaseAdapter {
 
 		private Context context;
@@ -138,7 +173,8 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 			}else{
 				holder = (MessageVeiwHolder) convertView.getTag();
 			}
-			
+
+			// 뷰에 데이터를 넣어주는 부분 
 			holder.getContentView().setText(data.getMessageContent());
 			SimpleDateFormat dateFormat = new SimpleDateFormat("a hh:mm");
 			holder.getTimeView().setText(dateFormat.format(data.getMessageTime()));
@@ -146,6 +182,7 @@ public class ChattingActivity extends ActionBarActivity implements OnClickListen
 			return convertView;
 		}
 		
+		// 어답터에 데이터 채워주는 함수
 		public void addItem(MessageData addItem){
 			this.messageListData.add(addItem);
 		}
---
+++ src/com/cbproject/caubook/sqlite/ConversationTable.java
@@ -0,0 +1,112 @@
+package com.cbproject.caubook.sqlite;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import com.cbproject.caubook.MessageTypeEnum;
+
+public class ConversationTable extends DBManager{
+	
+	public static final String tableName = "conversaton";
+	
+	// 대화 내용 테이블 원소 클래스
+	public static class ConversationData {
+		private int userID;
+		private int order;
+		private String content;
+		private Date time;
+		private MessageTypeEnum msgType;
+		
+		public ConversationData(int _id, int _order, String _content, Date _time, MessageTypeEnum _type) {
+			this.userID = _id;
+			this.order = _order;
+			this.content = _content;
+			this.time = _time;
+			this.msgType = _type;
+		}
+		
+		public int getUserID() { return this.userID; }
+		public int getOrder() { return this.order; }
+		public String getContent() { return this.content; }
+		public Date getTime() { return this.time; }
+		public MessageTypeEnum getMessageType() { return this.msgType; }
+	}
+	
+	// 대화내용 테이블 객체 생성자
+	public ConversationTable(Context context) {
+		super(context);
+	}
+	
+	// select 쿼리 함수
+	public ConversationData selectQuery(int keyID, int keyOrder) {
+		dbReader = dbOpenner.getReadableDatabase();
+		Cursor cursor = dbReader.query(tableName, null, null, null, null, null, null);
+		while(cursor.moveToNext()) {
+			// 합성키가 원하는 값하고 같은 경우 select
+			if(cursor.getInt(cursor.getColumnIndex("userID")) == keyID &&
+					cursor.getInt(cursor.getColumnIndex("orderNum")) == keyOrder) {
+				String strContent = cursor.getString(cursor.getColumnIndex("content"));
+				String strTime = cursor.getString(cursor.getColumnIndex("time"));
+				SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String strType = cursor.getString(cursor.getColumnIndex("msgType"));
+				try {
+					// 데이터 객체 만들어 리턴
+					return new ConversationData(keyID, keyOrder, strContent, sdFormat.parse(strTime), MessageTypeEnum.valueOf(strType));
+				} catch (ParseException e) {
+					Log.e("ParseError", "Conversation data parse error");
+					e.printStackTrace();
+					return null;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList<ConversationData> selectAllQuery() {
+		dbReader = dbOpenner.getReadableDatabase();
+		ArrayList<ConversationData> resultList = new ArrayList<ConversationTable.ConversationData>();
+		Cursor cursor = dbReader.query(tableName, null, null, null, null, null, null);
+		while(cursor.moveToNext()) {
+			int keyID = cursor.getInt(cursor.getColumnIndex("userID"));
+			int keyOrder = cursor.getInt(cursor.getColumnIndex("orderNum")); 
+			String strContent = cursor.getString(cursor.getColumnIndex("content"));
+			String strTime = cursor.getString(cursor.getColumnIndex("time"));
+			SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String strType = cursor.getString(cursor.getColumnIndex("msgType"));
+			try {
+				// 리스트에 추가
+				resultList.add(new ConversationData(keyID, keyOrder, strContent, sdFormat.parse(strTime), MessageTypeEnum.valueOf(strType)));
+			} catch (ParseException e) {
+				Log.e("ParseError", "Conversation data parse error");
+				e.printStackTrace();
+			}
+		}
+		return resultList;
+	}
+		
+	// insert 쿼리 함수
+	public boolean insertQuery(ConversationData data) {
+		dbWriter = dbOpenner.getWritableDatabase();
+		// insert 할 value 만들기
+		ContentValues val = new ContentValues();
+		val.put("userID", data.getUserID());
+		val.put("orderNum", data.getOrder());
+		val.put("content", data.getContent());
+		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		val.put("time", sdFormat.format(data.getTime()));
+		val.put("msgType", data.getMessageType().name());
+		
+		// 데이터 베이스 insert 쿼리 실행 결과 확인
+		if (dbWriter.insert(tableName, null, val) != -1) {
+			return true;
+		}
+		return false;
+	}
+}
